27de7ade66ebdc87dc994470c14966ff115c6cf5
Ulenn Terry Chern <utc2@njit.edu>
2016-02-23 00:59:17 -0500

    Split user creation into 2 parts, a form and a php file that handles the database insertions.

--- dbadmin/create-user.php
+++ dbadmin/create-user.php
@@ -1,97 +1,64 @@
-<html>
-<head>
-    <?php
-        include_once ('/dbtools/lists.php');
-        include_once ('/dbtools/db-info.php');
-        include_once ('/dbtools/backend.php');
-    ?>
-    <script type="text/javascript">
-        var xhttp;
-        if(window.XMLHttpRequest){
-            xhttp = new XMLHttpRequest();
-        } else {
-            // code for IE5/6
-            xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-    </script>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yoolen
+ * Date: 2/22/2016
+ * Time: 11:15 PM
+ */
+    include_once ("/dbtools/db-info.php");
+    $conn = new mysqli(SERVER, USERNAME, PASSWD, SCHEMA);
 
-</head>
-
-<body>
-<div id="logon">
-    <form action="adduser.php" method="POST">
-        <ul class="errorMsg"></ul>
-
-        <label for="usr">Username:&nbsp;</label>
-        <input type="text" name="usr"><br/>
-
-        <label for="fname">First Name:&nbsp;</label>
-        <input type="text" name="fname"><br/>
-
-        <label for="lname">Last Name:&nbsp;</label>
-        <input type="text" name="lname"><br/>
-
-        <label for="aff">Affiliation:&nbsp;</label>
-        <select name = "affiliation" id="aff">
-            <option value = ""></option>
-            <?php
-            $affils = getaffs();
-            foreach($affils as $affil):
-                echo '<option value="'.$affil[0].'">'.$affil.'</option>';
-            endforeach;
-            ?>
-        </select><br/>
-
-        <label for="email">Email:&nbsp;</label>
-        <input type="text" name="email"><br/>
-
-        <label for="phone">Phone:&nbsp;</label>
-        <input type="text" name="phone"><br/>
-
-        <label for="street1">Street 1:&nbsp;</label>
-        <input type="text" name="street1"><br/>
-
-        <label for="street2">Street 2:&nbsp;</label>
-        <input type="text" name="street2"><br/>
-
-        <label for="city">City:&nbsp;</label>
-        <input type="text" name="city"><br/>
-
-        <label for="state">State:&nbsp;</label>
-        <select name="state">
-            <option value=""></option>
-            <?php
-            foreach($states as $state):
-                echo '<option value="'.$state.'">'.$state.'</option>';
-            endforeach;
-            ?>
-        </select><br/>
-
-        <label for="zip">Zip:&nbsp;</label>
-        <input type="text" name="zip"><br/>
-
-        <label for="pwd">Password:&nbsp;</label>
-        <input type="password" name="pwd"><br/>
-
-        <label for="ul">User Level:&nbsp;</label>
-        <select name="userlevel" id="ulevel">
-            <option value=""></option>
-            <?php
-                foreach($userlevels as $userlevel):
-                    echo '<option value="'.$userlevel[0].'">'.$userlevel.'</option>';
-                    echo $userlevel[0];
-                endforeach;
-            ?>
-        </select><br/>
-    </form>
-    <button type="button" onclick="submit()">Submit</button>
-</div>
-<br>
-<script type="text/javascript">
-    function submit(){
-        var ulevel = document.getElementById('ulevel').value;
-        alert(ulevel);
+    $usr = $_POST['usr'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $aff = $_POST['aff'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $str1 = $_POST['street1'];
+    $str2 = $_POST['street2'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $zip = $_POST['zip'];
+    $passwd = $_POST['pwd'];
+    $cred = $_POST['ulevel'];
+    $date = date("Y-m-d H:i:s");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
-</script>
 
-</body>
+    $conn->autocommit(false);
+    if (isset($usrname) and isset($passwd) and isset($fname) and isset($lname)) {
+        if ($usr == '' or $passwd == '') {
+            echo "Bad Username/Password/Credentials";
+        } else {
+            $passhash = password_hash($passwd, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO authentication (username, passhash, permissions) VALUES (?, ?, ?)";
+            if ($stmt = $conn->prepare($sql)) {
+                $stmt->bind_param("ssi", $usrname, $passhash, $cred);
+                $stmt->execute();
+                $userid = $stmt->insert_id;
+                $stmt->close();
+                $success = ($userid != 0) ? true : false;
+            }
+            $sql = "INSERT INTO userinfo (user_FPK, fname, lname, affiliation, dept_FK, joindate) VALUES (?, ?, ?, ?, ?, ?)";
+            if ($stmt = $conn->prepare($sql)) {
+                $stmt->bind_param("isssis", $userid, $fname, $lname, $aff, $dept, $date);
+                $stmt->execute();
+                $stmt->close();
+                $success = ($userid != 0) ? true : false;
+            }
+            if ($success == true) {
+                $conn->commit();
+                echo "Successful insert.";
+            } else {
+                echo "Not inserted.";
+            }
+
+        }
+    } else {
+        echo "error";
+    }
+    $conn->close();
+    echo 'PHP finished';
+
+?>
