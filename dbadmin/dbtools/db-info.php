9802607a723767fcb881da0b60a173c2b21b299a
Ulenn Terry Chern <utc2@njit.edu>
2016-02-22 22:44:59 -0500

    Files with constants and arrays to be used.

---
+++ dbadmin/dbtools/db-info.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yoolen
+ * Date: 2/22/2016
+ * Time: 7:44 PM
+ */
+    define("SERVER", "initiateid.com:3306");
+    define("USERNAME", "cs490");
+    define("PASSWD", "projprojproj");
+    define("SCHEMA", "cs491");
+?>
